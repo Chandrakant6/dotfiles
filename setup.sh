0c33f45f0d6564f859c0920e317a5be86c9e2afd
chandu <141409100+Chandrakant6@users.noreply.github.com>
2026-01-20 17:59:22 +0530

    added pkg in setup.sh
    added cowsay, lolcat, fortune-mod

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ sudo apt-get update
 sudo apt-get upgrade -y
 echo "=========== [+] sucessfully Update and Upgrade =========="
 
-echo " === install essentials ===
-sudo apt-get install git curl build-essential tree python3 python3-pip vim htop toilet figlet cmatrix"
+echo " === install essentials === "
+sudo apt-get install git curl build-essential tree python3 python3-pip htop toilet figlet cmatrix cowsay lolcat fortune-mod
 
 cp .bashrc ~/.bashrc
 cp .bash_aliases ~/.bash_aliases
